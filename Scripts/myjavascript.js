3f8105b91d7391f73825bbcb59b9337519a38b93
Peter Connell <peterdconnell@hotmail.com>
2016-12-09 01:31:02 +0000

    final

--- Scripts/myjavascript.js
+++ Scripts/myjavascript.js
@@ -1,22 +1,21 @@
 ﻿//modified from http://thecodeplayer.com/walkthrough/matrix-rain-animation-html5-canvas-javascript
-
 function matrixRain() {
-
-
-    var c = document.getElementById("matrix");
-    var ctx = c.getContext("2d");
+    var canvas = document.getElementById("matrix");
+    var ctx = canvas.getContext("2d");
 
     //making the canvas full screen
-    c.height = window.innerHeight;
-    c.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    
+    
 
     //random characters - taken from the unicode charset
-    var randomCharacter = "019014";
+    var randomCharacter = "01010110";
     //converting the string into an array of single characters
     randomCharacter = randomCharacter.split("");
 
-    var font_size = 14;
-    var columns = c.width / font_size; //number of columns for the rain
+    var font_size = 12;
+    var columns = canvas.width / font_size; //number of columns for the rain
     //an array of drops - one per column
     var drops = [];
     //x below is the x coordinate
@@ -28,8 +27,8 @@ function matrixRain() {
     function draw() {
         //Black BG for the canvas
         //translucent BG to show trail
-        ctx.fillStyle = "rgba(0, 20, 51, 0.03)";
-        ctx.fillRect(0, 0, c.width, c.height);
+        ctx.fillStyle = "rgba(152, 180, 224, 0.15)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         ctx.fillStyle = "#ffffff"; //colour of text
         ctx.font = font_size + "px arial";
@@ -42,60 +41,57 @@ function matrixRain() {
 
             //sending the drop back to the top randomly after it has crossed the screen
             //adding a randomness to the reset to make the drops scattered on the Y axis
-            if (drops[i] * font_size > c.height && Math.random() > 0.975)
+            if (drops[i] * font_size > canvas.height && Math.random() > 0.975)
                 drops[i] = 0;
 
             //incrementing Y coordinate
             drops[i]++;
         }
     }
-    setInterval(draw, 46);
+    setInterval(draw, 82);
 
-    introduction();
-    //resultTable();
-    
+    function addAsBackground() {
+        var data = canvas.toDataURL();
+        var myElement = document.getElementById('myelement');
+        myElement.style.backgroundImage = 'url('+data+')'; 
+    }
+
+    addAsBackground();
 }
 
+// set rain effect as background
+function addAsBackground() {
+        var data = canvas.toDataURL();
+        var myElement = document.getElementById('myelement');
+        var height = myElement.style.height;
+        var width = myElement.style.width;
 
+        myElement.style.backgroundImage = 'url('+data+')';
+        alert(document.getElementById("myelement").style.backgroundImage);
+    }
+
+
+// Unused functions
 
 // canvas fade out to reveal cv
-function introduction() {
-    var pause = 3400;
-    var out = 3000;
-    var fadeIn = 4200
-    $("#matrix").delay(pause).fadeOut(out, "swing");
-    $(".default").delay(pause).show("highlight", { color: "#e6ffff" }, 2600);
-}
-
-// fade intro page to reveal cv
-function pageTransition() {
-    $("body").delay(5400).fadeOut(3000, "linear", redirectPage);
-
-    function redirectPage() {
-        window.location = "index.html";
-    }
-}
-
-// button events to show/hide divs
-
-function toggleSkills() { 
-    $('#Skills').toggle(600, "swing");
-}
-
-function toggleEducation() {
-    $('#Education').toggle(600, "swing");
-}
-
-function toggleProjects() {
-    $('#Projects').toggle(600, "swing");
-}
-
-function toggleWork() {
-    $('#Employment').toggle(600, "swing");
-}
+//function introduction() {
+//    var pause = 3400;
+//    var out = 3000;
+//    var fadeIn = 4200
+//    $("#matrix").delay(pause).fadeOut(out, "swing");
+//    $(".default").delay(pause).show("highlight", { color: "#e6ffff" }, 2600);
+//}
 
 
+//function multiCollapse() {
+//    $('.row-offcanvas').toggleClass('active');
+//    $('.collapse').toggleClass('in').toggleClass('hidden-xs').toggleClass('visible-xs');
+//}
 
+///* alert for contact form */
+//$('#contact_form').click(function () {
+//    alert('Message sent. Thank you.');
+//});
 
 
 
---
+++ angularFunctions.js
@@ -0,0 +1,87 @@
+﻿/* globals angular */
+
+//(function () {
+//    'use strict';
+
+//     Controller name is handy for logging
+//    var controllerId = 'angularFunctions';
+
+//     Define the controller on the module.
+//     Inject the dependencies. 
+//     Point to the controller definition function.
+//    angular.module('app').controller(controllerId, ['$scope', angularFunctions]);
+
+//    function angularFunctions($scope) {
+//         Using 'Controller As' syntax, so we assign this to the vm variable (for viewmodel).
+//        var vm = this;
+
+//         Bindable properties and functions are placed on vm.
+//        vm.activate = activate;
+//        vm.title = 'angularFunctions';
+
+//        function activate() {
+//        }
+
+//        #region Internal Methods  
+
+
+//        #endregion
+//    }
+//})();
+
+
+
+angular.module("myApp", [])
+    .controller("results", function ($scope) {
+
+        /*Year 1 results*/
+        $scope.modulesList1 = [
+            { module: "Operating System Fundamentals", result: "B+" },
+            { module: "Database Fundamentals", result: "A" },
+            { module: "Statistics", result: "A" },
+            { module: "Social Media Communications", result: "A" },
+            { module: "Object Orientated System Analysis", result: "A" },
+            { module: "Software Development 2", result: "A" },
+            { module: "Learning 2 Learn", result: "B-" },
+            { module: "Discrete Maths", result: "A" },
+            { module: "Business & Info Systems", result: "A" },
+            { module: "Fundamentals of Interface & Web", result: "A" },
+            { module: "Computer Architecture", result: "B+" },
+            { module: "Software Development 1", result: "A" }
+        ];
+        /*Year 2 results*/
+        $scope.modulesList2 = [
+            { module: "Software Development 3", result: "A" },
+            { module: "Software Development 4", result: "B+" },
+            { module: "Project", result: "A" },
+            { module: "Networking 1", result: "A" },
+            { module: "Databases", result: "A" },
+            { module: "Discrete Maths 2", result: "A" },
+            { module: "Web Design & Development 1", result: "B+" },
+            { module: "Networking 2 ", result: "A" },
+            { module: "Software Quality Ass & Test", result: "A" },
+            { module: "Management Science", result: "A" },
+            { module: "Information Security", result: "B+" },
+            { module: "Object Orientated Analysis", result: "B" }
+        ];
+        /*Year 3 results*/
+        $scope.modulesList3 = [
+            { module: "Innovation & Entrepreneurship", result: "A" },
+            { module: "Operating Systems", result: "B+" },
+            { module: "Web Design & Development  ", result: "B+" },
+            { module: "Algorithms and Data Structures", result: "B" },
+            { module: "Cloud servers and distributed computing", result: "B" },
+        ];
+        /*Year 4 results*/
+        $scope.modulesList4 = [
+            { module: "Enterprise Applications Architecture", result: "-" },
+            { module: "Enterprise Applications Development", result: "-" },
+            { module: "Information Management", result: "-" },
+            { module: "Social Media Analysis ", result: "-" },
+            { module: "Interactive Media Design", result: "-" },
+            { module: "Project", result: "-" }
+        ];
+                 
+       
+
+    });
